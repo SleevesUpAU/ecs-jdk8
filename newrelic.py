9f31f454205362ea8c1ea591716f55599f87cee3
Hasher <aravindhan.a@hashedin.com>
2018-12-31 22:38:16 +0530

    Changes in base python

--- newrelic.py
+++ newrelic.py
@@ -9,23 +9,23 @@ print (os.environ["VAULT_ADDR"])
 os.system('vault login -method=aws role=' + (os.environ["IAM_ROLE"]))
 
 newrelic_license_key = 'vault kv get -field=newrelic_license_key secret/new-relic'
-app_name = (os.environ["CONTAINER_NAME"])
-env = (os.environ["CONTAINER_ENVIRONMENT"])
+env = os.environ["ENVIRONMENT"]
+app_name = os.environ["CONTAINER_NAME"]
 
 def transform_newrelic_conf():
     with open('/newrelic.yml', 'r') as f:
         template = f.read()
 
     content = (
-        template.replace('__LICENSE_KEY__', subprocess.Popen(newrelic_license_key.split(" "), stdout=subprocess.PIPE,  shell=True).stdout.readline())                
-            .replace('__ENVIRONMENT_NAME__', '{}'.format(subprocess.Popen(env.split(" "), stdout=subprocess.PIPE,  shell=True).stdout.readline()))
-            .replace('__NEWRELIC_NAME__', '{}'.format(subprocess.Popen(app_name.split(" "), stdout=subprocess.PIPE, shell=True).stdout.readline()))
+        template.replace('__LICENSE_KEY__', subprocess.Popen(newrelic_license_key.split(" "), stdout=subprocess.PIPE).stdout.readline())              
+            .replace('__ENVIRONMENT_NAME__', env)
+            .replace('__NEWRELIC_NAME__', app_name)
             .replace('log_level: info', 'log_level: off')
             )
 
     with open('/newrelic.yml', 'w') as f:
-        f.write(str(content))
-        
+        f.write(content)
+
 def main():
     transform_newrelic_conf()
 
